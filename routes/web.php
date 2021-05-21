f83a04ca717905336ad46ccbfbdf2c0db50aca05
lavakush7800 <lavakushgiri@gmail.com>
2021-05-21 18:56:39 +0530

    frontend page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user', function () {
+    return view('user');
+});
 
 Auth::routes();
 
